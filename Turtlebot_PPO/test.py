dc50ee3a3d82b8ff106135bed5b02d3006c1d315
Tomas Mendoza <44282823+TomasMendozaHN@users.noreply.github.com>
2022-02-19 19:23:58 +0800

    Add files via upload

---
+++ Turtlebot_PPO/test.py
@@ -0,0 +1,8 @@
+import rospy
+from geometry_msgs.msg import Twist, Point, Quaternion
+from math import radians, copysign, sqrt, pow, pi, atan2
+import numpy as np
+import torch
+import tf
+
+print('ok!')
